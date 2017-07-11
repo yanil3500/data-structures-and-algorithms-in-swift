45c3f9bd45ea770521a8cb07d5880fc865dc2cf7
yanil3500 <yanil3500@gmail.com>
2017-07-11 11:40:28 -0700

    implements the push method on the Stack class.

--- data-structures/stack.swift
+++ data-structures/stack.swift
@@ -18,14 +18,34 @@ class Node<Element>{
 }
 
 class Stack<Element>: CustomStringConvertible {
-    var top : Node<Element>?
-    var description : String {
+    fileprivate var top : Node<Element>?
+    fileprivate var length : Int = 0
+    internal var description : String {
         var str : String = "[Stack] -> "
         var curr : Node? = top
         while curr != nil, let val = curr?.data {
             str += "[\(val)] -> "
+            curr = curr?.next
         }
         str += "[nil]"
         return str
+    }
+}
+
+extension Stack {
+    func push(data: Element){
+        /*
+         This method adds a new element to the top of the stack.
+         Complexity: O(1) Constant Time
+         */
+        if self.top == nil {
+            self.top = Node(data: data)
+            self.length += 1
+        } else {
+            let newItem : Node = Node(data: data)
+            newItem.next = self.top
+            self.top = newItem
+            self.length += 1
+        }
     }
 }
