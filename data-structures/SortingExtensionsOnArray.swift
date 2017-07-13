52e67b8f9631ea7f88d6cd73391877803970b80c
yanil3500 <yanil3500@gmail.com>
2017-07-13 15:10:56 -0700

    add selection sort implementation as an extension to Array

--- data-structures/SortingExtensionsOnArray.swift
+++ data-structures/SortingExtensionsOnArray.swift
@@ -32,3 +32,34 @@ extension Array where Iterator.Element: Comparable{
         return arr
     }
 }
+
+
+extension Array where Iterator.Element: Comparable {
+    /*
+     Selection sort is an in-place comparison-based sorting algorithm.
+     Selection sort runs in O(n^2), making it inefficient on a large collection of elements.
+     Selection sort does have some performance advantages over more complicated sorts in certain situations, particularly
+     where external memory is limited.
+     The sort divides the input collection into two parts: the subcollection of items already sorted, built from left to
+     right at the front of the collection, and, the subcollection of items remaining to be sorted that occupy the rest of
+     the list.
+     Algorithms proceeds by finding the smallest element in the unsorted subcollection, exchanging it with the leftmost
+     unsorted element (putting it in sorted order) and moving the sublist boundaries one element to the right.
+     */
+    mutating func selectionSort(){
+        for i in stride(from: 0, to: self.count, by: 1){
+            //Assumes the first element is the smallest.
+            var minIndex : Int = i
+            for j in stride(from: i + 1, to: self.count, by: 1){
+                //Tests against elements after j to find the smallest element.
+                if self[j] < self[minIndex]{
+                    minIndex = j
+                }
+            }
+            //swap teh value of the index number with new min
+            if minIndex != i {
+                swap(&self[i], &self[minIndex])
+            }
+        }
+    }
+}
