eeae7bfffbc31a1cb38f7f6bafb47679d8f461af
yanil3500 <yanil3500@gmail.com>
2017-07-19 14:56:52 -0700

    starts work on queue implementation; Adds QueueNode supporting class that inherits from an existing node class.

---
+++ data-structures/queue.swift
@@ -0,0 +1,17 @@
+//
+//  queue.swift
+//  dataStructures
+//
+//  Created by Elyanil Liranzo Castro on 7/19/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import Foundation
+
+class QueueNode<Element>: Node<Element>{
+    override init(data: Element) {
+        super.init(data: data)
+    }
+}
+
+
