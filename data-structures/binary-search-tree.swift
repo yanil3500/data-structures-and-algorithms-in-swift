341cbbf5a93822f85288a03b3a2ece219fb3a396
yanil3500 <yanil3500@gmail.com>
2017-07-20 18:12:35 -0700

    adds height property to BSTNode helper class; adds private recurse helper method that calculates the height of a BSTNode

--- data-structures/binary-search-tree.swift
+++ data-structures/binary-search-tree.swift
@@ -12,8 +12,22 @@ class BSTNode<Element>{
     var data : Element
     var left : BSTNode<Element>?
     var right : BSTNode<Element>?
+    var height : Int {
+        return self.getHeight()
+    }
     
     init(data: Element){
         self.data = data
+    }
+    
+    func getHeight() -> Int{
+        return getHeight(root: self)
+    }
+    private func getHeight(root: BSTNode?) -> Int {
+        if root == nil {
+            return 0
+        }
+        
+        return 1 + max(getHeight(root: root?.left), getHeight(root: root?.right))
     }
 }
