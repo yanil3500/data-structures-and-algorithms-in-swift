a59906dd88f443018f77e7f0227f25ed3f4663ab
yanil3500 <yanil3500@gmail.com>
2017-07-20 18:43:21 -0700

    adds class method responsible for rebalancing BST

--- data-structures/binary-search-tree.swift
+++ data-structures/binary-search-tree.swift
@@ -26,41 +26,62 @@ class BSTNode<Element>{
     func getHeight() -> Int{
         return getHeight(root: self)
     }
-    private func getHeight(root: BSTNode?) -> Int {
+    private func getHeight(root: BSTNode<Element>?) -> Int {
         if root == nil {
             return 0
         }
         return 1 + max(getHeight(root: root?.left), getHeight(root: root?.right))
     }
     
-    private func rightRotation(parent: BSTNode?) -> BSTNode?{
+    private func rightRotation(_ parent: BSTNode<Element>?) -> BSTNode<Element>?{
         // Performs a right rotation
-        let temp : BSTNode? = parent?.left
+        let temp : BSTNode<Element>? = parent?.left
         parent?.left = temp?.right
         temp?.right = parent
         return temp
     }
     
-    private func leftRotation(parent: BSTNode?) -> BSTNode?{
+    private func leftRotation(_ parent: BSTNode<Element>?) -> BSTNode<Element>?{
         //Performs a left rotation
-        let temp : BSTNode? = parent?.right
+        let temp : BSTNode<Element>? = parent?.right
         parent?.right = temp?.left
         temp?.left = parent
         return temp
     }
 
-    private func leftRightRotation(parent: BSTNode?) -> BSTNode?{
+    private func leftRightRotation(_ parent: BSTNode<Element>?) -> BSTNode<Element>?{
         // Performs a single leftRotation, and then a single rightRotation.
-        let temp : BSTNode? = parent?.left
-        parent?.left = leftRotation(parent: temp)
-        return rightRotation(parent: parent)
+        let temp : BSTNode<Element>? = parent?.left
+        parent?.left = leftRotation(temp)
+        return rightRotation(parent)
     }
     
-    private func rightLeftRotation(parent: BSTNode?) -> BSTNode?{
+    private func rightLeftRotation(_ parent: BSTNode<Element>?) -> BSTNode<Element>?{
         // Performs a single rightRotation, and then a single leftRotation.
-        let temp : BSTNode? = parent?.right
-        parent?.right = rightRotation(parent: temp)
-        return leftRotation(parent: parent)
+        let temp : BSTNode<Element>? = parent?.right
+        parent?.right = rightRotation(temp)
+        return leftRotation(parent)
+    }
+    
+    class func balance(temp: BSTNode<Element>?) -> BSTNode<Element>?{
+        guard let balanceFactor : Int = temp?.heightDifference else { return nil }
+        var newTemp : BSTNode<Element>? = temp
+        if balanceFactor > 1 {
+            //The BST is right heavy .
+            if let val : Int = temp?.left?.heightDifference, val > 0 {
+                newTemp = rightRotation(temp)
+            } else {
+                newTemp = leftRightRotation(temp)
+            }
+        } else if balanceFactor < -1 {
+            //The BST is left heavy.
+            if let val : Int = temp?.right?.heightDifference, val > 0 {
+                newTemp =  rightLeftRotation(temp)
+            } else {
+                newTemp = leftRotation(temp)
+            }
+        }
+        
     }
     
 }
