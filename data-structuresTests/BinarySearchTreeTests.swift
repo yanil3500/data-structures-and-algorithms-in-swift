1669c31595e79d3741f139931348c1dbe29e11fd
yanil3500 <yanil3500@gmail.com>
2017-07-21 19:13:47 -0700

    completes adding test cases for testInsert

--- data-structuresTests/BinarySearchTreeTests.swift
+++ data-structuresTests/BinarySearchTreeTests.swift
@@ -10,16 +10,16 @@ import XCTest
 @testable import dataStructures
 class BinarySearchTreeTests: XCTestCase {
     var testBST : BST<Double>!
-    var testCasesForInsert : [([Int], Int)]!
+    var testCasesForInsert : [([Double], Int)]!
     override func setUp() {
         super.setUp()
         testBST = BST<Double>()
         testCasesForInsert = [
-            (),
-            (),
-            (),
-            (),
-            ()
+            ([1, 6, 7,3, 1000, -1, -10, 4],8),
+            ([45, 67, 1, 2, 3, 4, 6, -9, 1, 21, -123, 234, 11212, -19],14),
+            ([7, -10, 35.6],3),
+            ([1.2],1),
+            ([56, 23.32, 23.1, 23.3455, 1294.13, -1],6)
         ]
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,7 +30,8 @@ class BinarySearchTreeTests: XCTestCase {
         testBST = nil
     }
     
-    func testInsertWorks() {
+    func testInsert() {
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
