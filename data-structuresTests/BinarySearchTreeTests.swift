2debb43c565feb5e6dd1e3427806f52e09131668
yanil3500 <yanil3500@gmail.com>
2017-07-25 15:20:26 -0700

    adds testCases for delete method

--- data-structuresTests/BinarySearchTreeTests.swift
+++ data-structuresTests/BinarySearchTreeTests.swift
@@ -11,6 +11,7 @@ import XCTest
 class BinarySearchTreeTests: XCTestCase {
     var testBST : BST<Double>!
     var testCasesForInsert : [([Double], Int)]!
+    var testCasesForDelete : [([Double], Int)]!
     override func setUp() {
         super.setUp()
         testBST = BST<Double>()
@@ -21,6 +22,16 @@ class BinarySearchTreeTests: XCTestCase {
             ([1.2],1),
             ([56, 23.32, 23.1, 23.3455, 1294.13, -1],6)
         ]
+        
+        testCasesForDelete = [
+            ([1, 6, 7,3, 1000, -1, -10, 4],8),
+            ([45, 67, 1, 2, 3, 4, 6, -9, 1, 21, -123, 234, 11212, -19],14),
+            ([7, -10, 35.6],3),
+            ([1.2],1),
+            ([56, 23.32, 23.1, 23.3455, 1294.13, -1],6)
+        ]
+        
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -55,12 +66,6 @@ class BinarySearchTreeTests: XCTestCase {
         }
     }
     
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
     
 }
