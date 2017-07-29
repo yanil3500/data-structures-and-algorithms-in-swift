5f7f499c6651c549bba44e9d8cb7e722927b2c34
yanil3500 <yanil3500@gmail.com>
2017-07-28 17:52:29 -0700

    adds tests that ensure hasVertex return the expected result when given a test value.

--- data-structuresTests/GraphTests.swift
+++ data-structuresTests/GraphTests.swift
@@ -10,9 +10,16 @@ import XCTest
 @testable import dataStructures
 class GraphTests: XCTestCase {
     var testGraph : Graph<String>!
+    var testCasesForHasVertex : Array<(source: String, destination: String, testVal: String, weight: Double, result: Bool)>!
     override func setUp() {
-        testGraph = Graph<String>()
         super.setUp()
+        testGraph = Graph<String>()
+        testCasesForHasVertex = [
+            (source: "Seattle", destination: "NYC", testVal:"NYC", weight: 1000, result: true),
+            (source: "Hong Kong", destination: "Moscow", testVal: "Bangor", weight: 2000 , result: false),
+            (source: "Cape Town", destination: "London", testVal: "Budapest", weight: 300 , result: false),
+            (source: "San Francisco", destination: "Tokyo", testVal: "Tokyo", weight: 3000, result: true)
+        ]
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -29,6 +36,14 @@ class GraphTests: XCTestCase {
         try? testGraph.addEdge(from: "Seattle", to: "New York City", weight: 1000)
         XCTAssertFalse(testGraph.isEmpty)
         
+    }
+    
+    func testHasVertex(){
+        //This function tests that hasVertex return the expected result when checking if the graph contains the testVal
+        for (source, dest, testVal, weight, result) in testCasesForHasVertex {
+            try? testGraph.addEdge(from: source, to: dest, weight: weight)
+            XCTAssertEqual(testGraph.hasVertex(from: testVal), result)
+        }
     }
     
     func testAddEdgeThrowsError(){
